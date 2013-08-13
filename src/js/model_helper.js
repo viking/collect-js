17403452faf11b1de2a1898f9f99d65b5a922cc5
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-08-13 15:47:48 -0500

    Convert Model to ModelHelper

---
+++ src/js/model_helper.js
@@ -0,0 +1,61 @@
+define(['lib/maria', 'util'], function(maria, util) {
+  var ModelHelper = function() {
+    this._attributes = {};
+  }
+
+  maria.borrow(ModelHelper.prototype, {
+    setAttribute: function(name, value, quiet) {
+      if (this._attributes[name] != value) {
+        this._attributes[name] = value;
+        if (!quiet) {
+          this.dispatchEvent({type: 'change'});
+        }
+        return true;
+      }
+      return false;
+    },
+
+    setAttributes: function(object) {
+      var changed = false;
+      for (var key in object) {
+        if (this.setAttribute(key, object[key], true)) {
+          changed = true;
+        }
+      }
+      if (changed) {
+        this.dispatchEvent({type: 'change'});
+      }
+    },
+
+    getAttributes: function() {
+      return this._attributes;
+    },
+  });
+
+  ModelHelper.extend = function(klass, options) {
+    options = options || {};
+    if (options.associations) {
+      for (var associationName in options.associations) {
+        var config = options.associations[associationName];
+        var setModel = config.setModel;
+        var getterName = 'get' + util.capitalize(associationName);
+        var variableName = '_' + associationName;
+
+        klass.prototype[getterName] = function() {
+          if (!this[variableName]) {
+            this[variableName] = new setModel();
+          }
+          return this[variableName];
+        }
+      }
+      klass.associations = {};
+      maria.borrow(klass.associations, options.associations);
+    }
+    if (options.entityName) {
+      klass.entityName = options.entityName;
+    }
+    maria.borrow(klass.prototype, ModelHelper.prototype);
+  };
+
+  return ModelHelper;
+});
---
+++ test/js/test_model_helper.js
@@ -0,0 +1,92 @@
+require(['lib/maria', 'model_helper'], function(maria, ModelHelper) {
+  function newSubclass(options) {
+    var namespace = {};
+    maria.Model.subclass(namespace, 'FooModel', {
+      constructor: function() {
+        maria.Model.apply(this, arguments);
+        ModelHelper.apply(this);
+      }
+    });
+    ModelHelper.extend(namespace.FooModel, options);
+    return namespace.FooModel;
+  }
+
+  buster.testCase('ModelHelper', {
+    "setAttribute": function() {
+      var klass = newSubclass();
+      var model = new klass();
+      model.setAttribute('foo', 123);
+      assert.equals(model.getAttributes(), {foo: 123})
+    },
+
+    "setAttributes": function() {
+      var klass = newSubclass();
+      var model = new klass();
+      model.setAttributes({foo: 123, bar: 'baz'});
+      assert.equals(model.getAttributes(), {foo: 123, bar: 'baz'})
+    },
+
+    "setAttribute triggers change event": function() {
+      var klass = newSubclass();
+      var model = new klass();
+      var spy = this.spy();
+      maria.on(model, "change", spy);
+
+      model.setAttribute("foo", 123);
+      assert.calledOnce(spy);
+    },
+
+    "setAttribute doesn't trigger change event for same value": function() {
+      var klass = newSubclass();
+      var model = new klass();
+      model.setAttribute("foo", 123);
+
+      var spy = this.spy();
+      maria.on(model, "change", spy);
+      model.setAttribute("foo", 123);
+      refute.called(spy);
+    },
+
+    "setAttributes triggers change event once": function() {
+      var klass = newSubclass();
+      var model = new klass();
+      var spy = this.spy();
+      maria.on(model, "change", spy);
+
+      model.setAttributes({foo: 123, bar: 'baz'});
+      assert.calledOnce(spy);
+    },
+
+    "setAttributes doesn't trigger change event for same value": function() {
+      var klass = newSubclass();
+      var model = new klass();
+      model.setAttributes({foo: 123, bar: 'baz'});
+
+      var spy = this.spy();
+      maria.on(model, "change", spy);
+      model.setAttributes({foo: 123, bar: 'baz'});
+      refute.called(spy);
+    },
+
+    "entityName": function() {
+      var klass = newSubclass({
+        entityName: 'foo'
+      });
+      assert.equals(klass.entityName, "foo");
+    },
+
+    "subclass with hasMany association": function() {
+      var setModel = this.spy();
+      var options = {
+        associations: {
+          bars: {type: 'hasMany', setModel: setModel, key: 'foo_id'}
+        }
+      };
+      var klass = newSubclass(options);
+      assert.equals(klass.associations, options.associations);
+
+      var foo = new klass();
+      var bars = foo.getBars();
+    }
+  });
+});
