8db8e3c73a8f743dfb714d71fa35109a27012909
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-08-20 10:49:56 -0500

    Make AdminProjectsInputView a subclass of InputView

--- src/js/views/admin/projects_input.js
+++ src/js/views/admin/projects_input.js
@@ -1,40 +1,15 @@
 define([
-  'lib/maria',
+  'input_view',
   'controllers/admin/projects_input',
-  'templates/admin/projects_input',
-  'route_helper'
-], function(maria, AdminProjectsInputController, AdminProjectsInputTemplate, RouteHelper) {
+  'templates/admin/projects_input'
+], function(InputView, AdminProjectsInputController, AdminProjectsInputTemplate) {
   var namespace = {};
 
-  maria.ElementView.subclass(namespace, 'AdminProjectsInputView', {
+  InputView.subclass(namespace, 'AdminProjectsInputView', {
     controllerConstructor: AdminProjectsInputController,
     template: AdminProjectsInputTemplate.source,
-    constructor: function() {
-      maria.ElementView.apply(this, arguments);
-      RouteHelper.mixin(this);
-    },
     uiActions: {
       'click input.create': 'onSubmit'
-    },
-    properties: {
-      reset: function() {
-        this.find('input').value = '';
-        var elts = this.findAll('.error');
-        for (var i = 0; i < elts.length; i++) {
-          elts[i].setAttribute('class', elts[i].getAttribute('class').replace(/ *error */, ''));
-        }
-      },
-
-      getValues: function() {
-        return({name: this.find('input').value});
-      },
-
-      displayErrors: function(errors) {
-        for (key in errors) {
-          var elt = this.find('.' + key);
-          elt.setAttribute('class', elt.getAttribute('class') + ' error');
-        }
-      }
     }
   });
 
