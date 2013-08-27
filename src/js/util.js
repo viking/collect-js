8149290176f426172a6fe8386d0d957a41eed465
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-08-27 17:56:54 -0500

    Move eventFire() to util module

--- src/js/util.js
+++ src/js/util.js
@@ -38,7 +38,17 @@ define(function() {
         delete object[key];
       }
     }
-  }
+  };
+
+  util.eventFire = function (el, etype) {
+    if (el.fireEvent) {
+      (el.fireEvent('on' + etype));
+    } else {
+      var evObj = document.createEvent('Events');
+      evObj.initEvent(etype, true, false);
+      el.dispatchEvent(evObj);
+    }
+  };
 
   return util;
 });
