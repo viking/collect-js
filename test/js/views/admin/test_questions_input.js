4470226dff676a0f71f4cc8aa0b91e59351ce9be
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2013-08-28 11:01:46 -0500

    Update AdminQuestionsInputView test to work with new test module

--- test/js/views/admin/test_questions_input.js
+++ test/js/views/admin/test_questions_input.js
@@ -1,5 +1,9 @@
-define(['models/questions', 'views/admin/questions_input'], function(QuestionsModel, AdminQuestionsInputView) {
-  buster.testCase('AdminQuestionsInputView', {
+define([
+  'lib/test',
+  'models/questions',
+  'views/admin/questions_input'
+], function(test, QuestionsModel, AdminQuestionsInputView) {
+  return new test.Suite('AdminQuestionsInputView', {
     setUp: function() {
       this.questions = new QuestionsModel();
       this.view = new AdminQuestionsInputView(this.questions);
@@ -10,8 +14,8 @@ define(['models/questions', 'views/admin/questions_input'], function(QuestionsMo
       this.view.find('input.name').value = 'foo';
       this.view.find('select.type').selectedIndex = 0;
       this.view.find('input.prompt').value = 'Foo:';
-      assert.equals(this.view.getValues(), {
-        name: 'foo', type: 'text', prompt: "Foo:", form_id: 123
+      this.assertEquals(this.view.getValues(), {
+        name: 'foo', type: 'text', prompt: "Foo:", form_id: "123" // XXX: should this be a string?
       });
     },
 
@@ -25,9 +29,9 @@ define(['models/questions', 'views/admin/questions_input'], function(QuestionsMo
       prompt.value = 'Foo:'
 
       this.view.reset();
-      assert.equals(name.value, '');
-      assert.equals(type.selectedIndex, 0);
-      assert.equals(prompt.value, '');
+      this.assertEquals(name.value, '');
+      this.assertEquals(type.selectedIndex, 0);
+      this.assertEquals(prompt.value, '');
     }
   });
 });
--- test/main.js
+++ test/main.js
@@ -45,6 +45,7 @@ require([
     'test/views/admin/test_projects_list',
     'test/views/admin/test_question_item',
     'test/views/admin/test_questions',
+    'test/views/admin/test_questions_input',
   ], function() {
     for (var i = 0; i < arguments.length; i++) {
       runner.addSuite(arguments[i]);
